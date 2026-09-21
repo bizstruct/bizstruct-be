6205cadc829d6b3681a5d02be75198bf2e8eb9f8
Yaroslav Markivskyi <yaroslavmarkivskyi@gmail.com>
2026-09-21 19:06:36 +0000

    feat(stage): add Stage model and service for project stages management

---
+++ app/services/stage.py
@@ -0,0 +1,16 @@
+
+
+class StageService:
+    
+    
+    def create_stages():
+        """
+            Create all stages for a project.
+        """
+        ...
+    
+    def run_stage():
+        """
+            Run a specific stage for a project.
+        """
+        ...
